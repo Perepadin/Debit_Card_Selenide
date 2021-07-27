53f9e2f2d7451848dad8d020cc4bd8bc4a1d8856
Perepadin <82140378+Perepadin@users.noreply.github.com>
2021-07-27 18:30:34 +0300

    should test pass

--- src/test/java/DebitCardTest.java
+++ src/test/java/DebitCardTest.java
@@ -1,10 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -13,35 +9,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardTest {
 
-    // будет использован селенид
-
-    private WebDriver driver;
-
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
-    void shouldTestV1() {
+    void shouldTestPassed() {
         open("http://localhost:7777");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Александр Иванов");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иван Васильевич Царь");
         form.$("[data-test-id=phone] input").setValue("+79270000001");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
